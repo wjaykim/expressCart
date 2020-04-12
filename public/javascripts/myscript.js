6bea603f033511e395f23d7aa197944cfcd2420d
a1p4ca <dreamaker7770@gmail.com>
2020-04-12 09:32:31 +0900

    delay

--- public/javascripts/myscript.js
+++ public/javascripts/myscript.js
@@ -1,5 +1,9 @@
 /* eslint-disable prefer-arrow-callback, no-var, no-tabs */
-var script = document.createElement('script');
-script.src = '/javascripts/myscript-final.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('body')[0].appendChild(script);
+var delay = 0
+
+setTimeout(function() {
+    var script = document.createElement('script')
+    script.src = '/js/myscript-final.js'
+    script.type = 'text/javascript'
+    document.getElementsByTagName('body')[0].appendChild(script)
+}, delay)
